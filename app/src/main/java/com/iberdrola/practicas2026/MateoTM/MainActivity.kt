b8932b005eaa1fe803e4ecc15c3d28fba0823215
Mateo Tulian <mateotulian5@gmail.com>
2026-05-07 12:56:23 +0200

    Implementación de la tarjeta de Última Factura

--- app/src/main/java/com/iberdrola/practicas2026/MateoTM/MainActivity.kt
+++ app/src/main/java/com/iberdrola/practicas2026/MateoTM/MainActivity.kt
@@ -4,16 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBackIosNew
+import androidx.compose.material.icons.outlined.Lightbulb
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -34,6 +42,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -75,8 +84,12 @@ fun FacturasScreen(exit: () -> Unit){
             Cabecera(direccion = "C/ Palma - ARTA KM 49,5,4ºA - PINTO - MADRID")
 
             Spacer(modifier = Modifier.height(15.dp))
-
             LuzGasTabs()
+
+            Spacer(modifier = Modifier.height(5.dp))
+            UltimaFactura()
+
+
         }
 
     }
@@ -176,6 +189,81 @@ fun Cabecera(direccion: String){
         fontSize = 20.sp,
         fontWeight = FontWeight.Bold
     )
+}
+
+@Composable
+fun UltimaFactura(){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        shape = RoundedCornerShape(20.dp),
+        border = BorderStroke(1.dp, color = Color(0xFF096E19)),
+        colors = CardDefaults.cardColors(Color.White)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .padding(horizontal = 20.dp, vertical = 15.dp)
+        ) {
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Última factura",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Outlined.Lightbulb,
+                    tint = Color(0xFF096E19),
+                    contentDescription = "Bombilla",
+                    modifier = Modifier.size(40.dp).offset(y = 10.dp)
+
+
+                )
+            }
+
+            //Spacer(modifier = Modifier.padding(vertical = 1.dp))
+            Text(text = "Factura Luz")
+
+            Spacer(modifier = Modifier.height(15.dp))
+            Text(
+                text = "30,00 €",
+                fontSize = 25.sp,
+                fontWeight = FontWeight.ExtraBold
+            )
+
+            Spacer(modifier = Modifier.height(2.dp))
+            Text(text = "01 feb. 2024 - 04 mar.2024")
+
+            HorizontalDivider(
+                modifier = Modifier.padding(vertical = 20.dp),
+                color = Color(0xFF70968B)
+            )
+
+            Card(
+                modifier = Modifier.height(30.dp).width(135.dp),
+                colors = CardDefaults.cardColors(Color(0xFFE18F8F))
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        modifier = Modifier.fillMaxWidth().padding(vertical = 6.dp),
+                        text = "Pendiente de pago",
+                        textAlign = TextAlign.Center,
+                        color = Color(0xFF5E1414),
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
