a6cd7d1abb00f7df758cd47c14afb624f11a5aa5
Mateo Tulian <mateotulian5@gmail.com>
2026-05-11 13:16:38 +0200

    Incluí una excepción en ListadoFacturasViewModelFactory por si me falla que no me explote la app

--- app/src/main/java/com/iberdrola/practicas2026/MateoTM/ui/listado_facturas/ListadoFacturasViewModelFactory.kt
+++ app/src/main/java/com/iberdrola/practicas2026/MateoTM/ui/listado_facturas/ListadoFacturasViewModelFactory.kt
@@ -8,6 +8,9 @@ import com.iberdrola.practicas2026.MateoTM.model.FacturaRespository
 // Esta clase me sirve para poder crear el ViewModel porque ya tiene un parámetro, si está vacío no hace falta
 class ListadoFacturasViewModelFactory (private val repository: FacturaRespository) : ViewModelProvider.Factory{
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return ListadoFacturasViewModelFactory(repository) as T
+        if (modelClass.isAssignableFrom(ListadoFacturasViewModel::class.java)) {
+            return ListadoFacturasViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Clase viewmodel desconocida")
     }
 }
