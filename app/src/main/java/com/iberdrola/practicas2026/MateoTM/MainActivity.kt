396603ecc02d8ca61e4a908da23131dca5482862
Mateo Tulian <mateotulian5@gmail.com>
2026-05-11 14:58:38 +0200

    Implementación de Hilt para la inyección del ViewModel en MainActivity.

--- app/src/main/java/com/iberdrola/practicas2026/MateoTM/MainActivity.kt
+++ app/src/main/java/com/iberdrola/practicas2026/MateoTM/MainActivity.kt
@@ -4,69 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ArrowBackIosNew
-import androidx.compose.material.icons.outlined.Lightbulb
-import androidx.compose.material.icons.outlined.Tune
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SecondaryScrollableTabRow
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRowDefaults
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.iberdrola.practicas2026.MateoTM.model.FacturaRespository
-import com.iberdrola.practicas2026.MateoTM.ui.listado_facturas.ListadoFacturasContent
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.iberdrola.practicas2026.MateoTM.ui.listado_facturas.ListadoFacturasScreen
-import com.iberdrola.practicas2026.MateoTM.ui.listado_facturas.ListadoFacturasViewModel
-import com.iberdrola.practicas2026.MateoTM.ui.listado_facturas.ListadoFacturasViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val respositorio = FacturaRespository(this)
-        // aca creo el viewModel para usar la factory que es quien se encarga de crearlo
-        val viewModel: ListadoFacturasViewModel by viewModels {
-            ListadoFacturasViewModelFactory(respositorio)
-        }
+
         setContent {
-            ListadoFacturasScreen(viewModel = viewModel, onExitClick = { finish() } ) // los parametros que le entran a la Screen
+            ListadoFacturasScreen(
+                viewModel = hiltViewModel(),
+                onExitClick = { finish() }
+            )
         }
     }
 }
