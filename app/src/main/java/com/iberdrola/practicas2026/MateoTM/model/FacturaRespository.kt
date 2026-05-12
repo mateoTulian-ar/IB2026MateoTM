8686a409219163a77a8521771f94d9118cd07043
Mateo Tulian <mateotulian5@gmail.com>
2026-05-12 13:05:53 +0200

    Implementación de un tiempo aleatorio en el repositorio de facturas sin uso.

--- app/src/main/java/com/iberdrola/practicas2026/MateoTM/model/FacturaRespository.kt
+++ app/src/main/java/com/iberdrola/practicas2026/MateoTM/model/FacturaRespository.kt
@@ -5,11 +5,14 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.milliseconds
 
 class FacturaRespository @Inject constructor(
     @ApplicationContext val context: Context
 ) {
     fun facturasJSON() : List<Factura>{
+
+        val tiempoAlea = (1000..3000).random().milliseconds
         val json = context.assets.open("data/facturas.json").bufferedReader().use { it.readText() }
 
         val gson = Gson()
