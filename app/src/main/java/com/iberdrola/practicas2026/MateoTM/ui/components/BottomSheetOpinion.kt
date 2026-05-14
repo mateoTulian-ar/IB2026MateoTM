88bbaa3a418b4fcd032ff2d5d4ca1afb589abac7
Mateo Tulian <mateotulian5@gmail.com>
2026-05-14 13:57:24 +0200

    Implementación del componente BottomSheet de Opinión con iconos de satisfacción.

---
+++ app/src/main/java/com/iberdrola/practicas2026/MateoTM/ui/components/BottomSheetOpinion.kt
@@ -0,0 +1,142 @@
+package com.iberdrola.practicas2026.MateoTM.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.SentimentDissatisfied
+import androidx.compose.material.icons.outlined.SentimentNeutral
+import androidx.compose.material.icons.outlined.SentimentSatisfiedAlt
+import androidx.compose.material.icons.outlined.SentimentVeryDissatisfied
+import androidx.compose.material.icons.outlined.SentimentVerySatisfied
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheetOpinion(
+    onDismiss: () -> Unit,
+    onMasTarde: () -> Unit
+) {
+
+    ModalBottomSheet(
+        onDismissRequest = onDismiss,
+        containerColor = Color.White,
+        dragHandle = { BottomSheetDefaults.DragHandle(color = Color.Gray) }
+
+    )
+    {
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Tu opinión nos importa",
+                modifier = Modifier.padding(top = 5.dp),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "¿Cómo de probable es que recomiendes esta app a amigos o familiares " +
+                        "para que realicen sus gestiones?",
+                modifier = Modifier.padding(horizontal = 24.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 16.sp,
+                color = Color.Gray,
+                lineHeight = 20.sp
+            )
+
+            //Spacer(modifier = Modifier.height(5.dp))
+
+            HorizontalDivider(
+                modifier = Modifier.fillMaxWidth().padding(18.dp),
+                color = Color.LightGray
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(top = 10.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.SentimentVeryDissatisfied,
+                    modifier = Modifier.clickable {},
+                    contentDescription = null,
+                    tint = Color(0xFFC7392E)
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                Icon(
+                    imageVector = Icons.Outlined.SentimentDissatisfied,
+                    modifier = Modifier.clickable {},
+                    contentDescription = null,
+                    tint = Color(0xFFFFC107)
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                Icon(
+                    imageVector = Icons.Outlined.SentimentNeutral,
+                    modifier = Modifier.clickable {},
+                    contentDescription = null,
+                    tint = Color.Gray
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                Icon(
+                    imageVector = Icons.Outlined.SentimentSatisfiedAlt,
+                    modifier = Modifier.clickable {},
+                    contentDescription = null,
+                    tint = Color(0xFF1973BD)
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                Icon(
+                    imageVector = Icons.Outlined.SentimentVerySatisfied,
+                    modifier = Modifier.clickable {},
+                    contentDescription = null,
+                    tint = Color(0xFF2E6930)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(15.dp))
+            Text(
+                text = "Responder más tarde",
+                modifier = Modifier.clickable { onMasTarde() },
+                color = Color(0xFF245226),
+                textDecoration = TextDecoration.Underline
+
+            )
+
+
+        }
+    }
+
+}
+
+@Composable
+@Preview (showBackground = true, showSystemUi = true)
+fun Preview(){
+    BottomSheetOpinion(
+        onDismiss = {},
+        onMasTarde = {})
+}
